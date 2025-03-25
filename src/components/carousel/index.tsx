e23252e4b6b50f05478e5baed0dee806da638e16
Alexander <alexander@Mac.hitronhub.home>
2025-03-24 20:47:44 -0700

    change

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Animated, View, Image, Easing } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Image, StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux'
 import { IStores } from '../../state/store'
 
@@ -10,64 +10,74 @@ interface ICarouselProps {
 function Carousel({ images }: ICarouselProps) {
   const systemStore = useSelector((state: IStores) => state.systemStore)
   const { mobile } = systemStore
-  const { Colors } = systemStore.mobile ? systemStore.Mobile : systemStore.Desktop
+  const { Colors, Fonts, Spacing } = systemStore.mobile ? systemStore.Mobile : systemStore.Desktop
 
-  const translateX = useRef(new Animated.Value(0)).current
   const [currentIndex, setCurrentIndex] = useState(0)
-  const [width, setWidth] = useState(0)
-  const [height, setHeight] = useState(0)
-
-  const animate = (toValue: number) => {
-    Animated.timing(translateX, {
-      toValue: -toValue,
-      duration: 500,
-      easing: Easing.inOut(Easing.ease),
-      useNativeDriver: true,
-    }).start()
-  }
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 })
 
   useEffect(() => {
-    if (!width) return
+    if (!dimensions.width) return
 
     const interval = setInterval(() => {
-      const nextIndex = (currentIndex + 1) % images.length
-      setCurrentIndex(nextIndex)
-      animate(nextIndex * width)
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
     }, 3000)
 
     return () => clearInterval(interval)
-  }, [images, width, currentIndex])
+  }, [images, dimensions.width])
+
+  const handleLayout = (event: any) => {
+    const { width, height } = event.nativeEvent.layout
+    setDimensions({ width, height })
+  }
 
   return (
     <View
-      style={{flex: 1, overflow: 'hidden', backgroundColor: Colors.black}}
-      onLayout={(event) => {
-        const { width, height } = event.nativeEvent.layout
-        setWidth(width)
-        setHeight(height)
-      }}
+      style={[
+        styles.container, 
+        { backgroundColor: Colors.black }
+      ]}
+      onLayout={handleLayout}
     >
-      <Animated.View 
-        style={{
-          flexDirection: 'row', 
-          width: images.length * width,
-          transform: [{translateX}]
-        }}
+      <View 
+        style={[
+          styles.carouselContent, 
+          { 
+            width: images.length * dimensions.width,
+            transform: [{ 
+              translateX: -currentIndex * dimensions.width 
+            }]
+          }
+        ]}
       >
         {images.map((img, i) => (
           <Image
             key={i}
             source={{ uri: img }}
-            style={{
-              width: width, 
-              height: height, 
-              resizeMode: 'cover'
-            }}
+            style={[
+              styles.image,
+              { 
+                width: dimensions.width, 
+                height: dimensions.height 
+              }
+            ]}
           />
         ))}
-      </Animated.View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: 'hidden'
+  },
+  carouselContent: {
+    flexDirection: 'row'
+  },
+  image: {
+    resizeMode: 'cover'
+  }
+})
 
 export default Carousel
